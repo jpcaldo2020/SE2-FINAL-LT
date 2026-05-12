a1be84e24bc3d6ad4b2123c25f38e5d8b21bcf1a
jpcaldo2020 <jocaldo@my.cspc.edu.ph>
2026-05-12 21:54:50 +0800

    try

--- app.py
+++ app.py
@@ -10,4 +10,5 @@ def hello_world():
 
 # Run the app if this script is executed
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 8880))
+    app.run(host="0.0.0.0", port=port)
